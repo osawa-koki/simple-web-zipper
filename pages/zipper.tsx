00243f7d7a4e09f3a30a6ca67b9916e34d226c1f
osawa-koki <koki@osawa.dev>
2023-12-14 21:45:07 +0900

    ファイルを選択可能に＆一覧表示

--- pages/zipper.tsx
+++ pages/zipper.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Table } from 'react-bootstrap'
+import { Alert, Form } from 'react-bootstrap'
 
 export default function Zipper (): React.JSX.Element {
+  const [files, setFiles] = useState<FileList | null>(null)
+
   return (
     <>
+      <Form.Group controlId="formBasicFiles">
+        <Form.Label>Select Files</Form.Label>
+        <Form.Control type="file" multiple onChange={(event) => {
+          const inputElement = event.target as HTMLInputElement
+          setFiles(inputElement.files)
+        }} />
+        <Form.Text className="text-muted">
+          Select files to be zipped
+        </Form.Text>
+      </Form.Group>
+      <hr />
+      {
+      files == null
+        ? (
+         <Alert variant="warning">
+           No files selected.
+          </Alert>
+          )
+        : (
+          <Table>
+            <thead>
+              <tr>
+                <th>File Name</th>
+                <th>File Size</th>
+                <th>File Type</th>
+                <th>File Last Modified</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                Array.from(files).map((file, index) => (
+                  <tr key={index}>
+                    <td>{file.name}</td>
+                    <td>{file.size}</td>
+                    <td>{file.type}</td>
+                    <td>{file.lastModified}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </Table>
+          )
+      }
     </>
   )
 }
